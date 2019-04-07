c40e9a7f6a8ad9966a9d4b5a1e9d9329a03b0380
DavidSandor <sandor.david89@gmail.com>
2019-04-07 19:05:51 +0200

    App init

---
+++ src/Containers/TileContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class TileContainer extends Component {
+
+    render() {
+        return (
+          <div></div>
+        );
+      }
+}
+
+export default TileContainer;
